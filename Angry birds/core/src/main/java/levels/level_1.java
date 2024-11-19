8d447abe6dfd25dae33e065e3405941b9671f41a
sankalp484 <sankalp23484@iiitd.ac.in>
2024-11-20 02:15:05 +0530

    Update level_1.java

--- Angry birds/core/src/main/java/levels/level_1.java
+++ Angry birds/core/src/main/java/levels/level_1.java
@@ -29,7 +29,7 @@ public class level_1 extends MainLevel {
 
         float groundY = 300;
         createGlassSetup(glassBlocks, pigs, 900, groundY);
-        createGlassSetup(glassBlocks, pigs, 1200, groundY);
+        createGlassSetup(glassBlocks, pigs, 1300, groundY);
 
         setupLevel(birds, glassBlocks, pigs);
     }
@@ -40,7 +40,9 @@ public class level_1 extends MainLevel {
         pigs.add(new SmallPig(this, startX + 102, groundY + 20));
         glassBlocks.add(new Glass(this, startX, groundY + 204, 0));
         glassBlocks.add(new Glass(this, startX + 200, groundY + 204, 0));
+
     }
+
 
     @Override
     public void render(float delta) {
